33360e5f6743f1d3ccd24a0008fcd163d50d9ed5
mjmersenski <mjmersenski@10.30.22.247>
2017-11-01 21:48:05 -0500

    created one toString method for a LocalDate object

--- src/student/lab/MJMDateUtilities.java
+++ src/student/lab/MJMDateUtilities.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package student.lab;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
@@ -11,4 +9,29 @@ package student.lab;
  */
 public class MJMDateUtilities {
     
+        /**
+     * Format a <code>LocalDate</code> to a month/day/year format (mm/dd/yyyy)
+     *
+     * @param date - a <code>LocalDate</code> object
+     * @return a date formatted to month/day/year (mm/dd/yyyy)
+     * @throws IllegalArgumentException if date is null
+     */
+    public String toString(LocalDate date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        return date.format(formatter);
+        // add an exception
+    }
+    
+    public String toString(){
+        
+        return "";
+    }
+    
+    public static void main(String[] args) {
+        MJMDateUtilities mjm = new MJMDateUtilities();
+        LocalDate now = LocalDate.now();
+        
+        String test = mjm.toString(now);
+        System.out.println(test);
+    }
 }
