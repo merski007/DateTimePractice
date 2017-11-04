03a1386f7dd1071ebf0f2a70ae1902cdaa30669a
mjmersenski <mjmersenski@10.30.22.247>
2017-11-04 11:35:28 -0500

    converted all my methods to LocalDateTime object instead of LocalDate objects to give the program more flexibility.

--- src/student/lab/MJMDateUtilities.java
+++ src/student/lab/MJMDateUtilities.java
@@ -20,31 +20,31 @@ import java.util.Calendar;
 public class MJMDateUtilities {
 
     /**
-     * Format a <code>LocalDate</code> to a month/day/year format (mm/dd/yyyy)
+     * Format a <code>LocalDateTime</code> to a month/day/year hour:minute format (mm/dd/yyyy HH:mm)
      *
-     * @param date a <code>LocalDate</code> object
-     * @return a date formatted to month/day/year (mm/dd/yyyy)
+     * @param date a <code>LocalDateTime</code> object
+     * @return a date formatted to month/day/year hour:minute (mm/dd/yyyy HH:mm)
      * @throws IllegalArgumentException if date is null
      */
-    public String toString(LocalDate date) throws IllegalArgumentException {
+    public String toString(LocalDateTime date) throws IllegalArgumentException {
         if (date == null) {
             throw new IllegalArgumentException("Error: date argument cannot be null");
         }
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm");
         return date.format(formatter);
         // add an exception
     }
 
     /**
-     * Format a <code>Date</code> according to a specified format
+     * Format a <code>LocalDateTime</code> object according to a specified format
      *
-     * @param date a <code>Date</code> object
-     * @param pattern a String with the specified date/time pattern
-     * @return a date and/or time formatted according to the specified pattern
+     * @param date a <code>LocalDateTime</code> object
+     * @param pattern a String with the specified date & time pattern
+     * @return a date and/or time format according to the specified pattern
      * @throws IllegalArgumentException if pattern is not recognized and/or date
      * is null
      */
-    public String toString(LocalDate date, String pattern) throws IllegalArgumentException {
+    public String toString(LocalDateTime date, String pattern) throws IllegalArgumentException {
         if (date == null) {
             throw new IllegalArgumentException("Error: date argument cannot be null");
         }
@@ -53,42 +53,42 @@ public class MJMDateUtilities {
     }
 
     /**
-     * Attempts to convert a String representation of a date to a
-     * java.time.LocalDate object. Conversion rules are based on parse
+     * Attempts to convert a String representation of a date & time to a
+     * java.time.LocalDateTime object. Conversion rules are based on parse
      * definitions in the java.​time.​format.​DateTimeFormatter class.
      *
      * @param dateString a string representation of a date
-     * @return a java.time.LocalDate object
+     * @return a java.time.LocalDateTime object
      * @throws IllegalArgumentException if the date string cannot be parsed
-     * and/or if the value is null or empty.
+     * and/or if the string object is null or empty.
      */
-    public LocalDate toDate(String dateString) throws IllegalArgumentException {
+    public LocalDateTime toDateTime(String dateString) throws IllegalArgumentException {
         if (dateString == null || dateString.isEmpty()) {
             throw new IllegalArgumentException("Error: dateString argument cannot be null or empty");
         }
 
-        LocalDate date = LocalDate.parse(dateString);
+        LocalDateTime date = LocalDateTime.parse(dateString);
         return date;
     }
 
      /**
      * Attempts to convert a String representation of a date to a
-     * java.time.LocalDate object by with any pattern specified. 
+     * java.time.LocalDateTime object by with any pattern specified. 
      * Conversion rules are based on parse definitions in the 
      * java.​time.​format.​DateTimeFormatter class.
      *
-     * @param dateString a string representation of a date
-     * @param pattern a String with the specified date/time pattern
-     * @return a java.time.LocalDate object
+     * @param dateString a string representation of a date & time
+     * @param pattern a String with the specified date & time pattern
+     * @return a java.time.LocalDateTime object
      * @throws IllegalArgumentException if the date string cannot be parsed
-     * and/or if the value is null or empty.
+     * and/or if the string object is null or empty.
      */
-    public LocalDate toDate(String dateString, String pattern) throws IllegalArgumentException {
+    public LocalDateTime toDateTime(String dateString, String pattern) throws IllegalArgumentException {
         if (dateString == null || dateString.isEmpty()) {
             throw new IllegalArgumentException("Error: dateString argument cannot be null or empty");
         }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
-        LocalDate date = LocalDate.parse(dateString, formatter);
+        LocalDateTime date = LocalDateTime.parse(dateString, formatter);
 
         return date;
     }
@@ -134,32 +134,28 @@ public class MJMDateUtilities {
         MJMDateUtilities mjm = new MJMDateUtilities();
 
 //        // toString test - outputs a formatted string MM/dd/yyyy
-//        LocalDate today = LocalDate.now();
-//        System.out.println(mjm.toString(today));
+        LocalDateTime today = LocalDateTime.now();
+        System.out.println(mjm.toString(today));
 
 //        // toString test - takes a date and format as parameters and returns specified format
-//        LocalDate today = LocalDate.now();
-//        String pattern = "yyyy/MM/dd";
+//        LocalDateTime today = LocalDateTime.now();
+//        String pattern = "yyyy/MM/dd HH:mm";
 //        System.out.println(mjm.toString(today, pattern));
 
-//        // toDate test - takes a string as a paramter
-//        String today = "2017-11-02";
-//        mjm.toDate(today);
-//        System.out.println(mjm.toDate(today).getDayOfWeek());
+//        // toDateTime test - takes a string as a paramter
+//        String today = "2007-11-03T12:30:00";
+//        mjm.toDateTime(today);
+//        System.out.println(mjm.toDateTime(today));
 
-        // toDate test - takes a string date and a string format
-//        String today = "11/03/2017";
-//        String pattern = "MM/dd/yyyy";
-//        System.out.println(mjm.toDate(today, pattern).getDayOfWeek());
+//         toDateTime test - takes a string date and a string format
+//        String today = "11/03/2017 12:30";
+//        String pattern = "MM/dd/yyyy HH:mm";
+//        System.out.println(mjm.toDateTime(today, pattern));
 
         // getDateDiff - 
-        LocalDateTime date = LocalDateTime.now();
-        LocalDateTime yesterday = date.minusDays(1);
-        
-//        mjm.getDateDiff(yesterday, date, MINUTES);
-        System.out.println(mjm.getDateDiff(yesterday, date, DAYS));
-        
-        
+//        LocalDateTime date = LocalDateTime.now();
+//        LocalDateTime yesterday = date.minusDays(1);
+//        System.out.println(mjm.getDateDiff(yesterday, date, DAYS));
 
     }
 }
